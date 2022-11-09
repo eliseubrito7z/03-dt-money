38e6e8978870033cdaf228970a98ab8daf689ba7
eliseubrito7z <eliseubrito776@gmail.com>
2022-11-08 21:56:26 -0300

    interface finalizada

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { Transations } from "./pages/Transations";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+
+  return ( 
+    <ThemeProvider  theme={defaultTheme}>
+      <GlobalStyle />
+      <Transations />
+    </ThemeProvider>
+  )
+    
+}
+
